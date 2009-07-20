e22914dab607c93cf5241fdad971fd1e20d8e51b
Darren Johnson <djohnson@volitant.haskell.unr.edu>
2009-07-20 14:58:44 -0700

    inventory_units now associate with the appropriate order when sold and inventory is on hand

--- app/models/inventory_unit.rb
+++ app/models/inventory_unit.rb
@@ -45,6 +45,7 @@ class InventoryUnit < ActiveRecord::Base
       on_hand = self.retrieve_on_hand(variant, quantity)
       # mark all of these units as sold and associate them with this order 
       on_hand.each do |unit|
+        unit.order = order
         unit.sell!
       end
       # right now we always allow back ordering
